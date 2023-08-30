eb71f1ef70bd7730c43f8f094d544a942159dde3
Cheese Curd <51841108+Cheese-Curd@users.noreply.github.com>
2023-08-30 19:44:02 -0400

    ALL THE DRINKS AAAAAA-

--- src/main/java/io/github/cheesecurd/vallhalla/Items.java
+++ src/main/java/io/github/cheesecurd/vallhalla/Items.java
@@ -44,11 +44,42 @@ public class Items
 
 	// Drinks
 
+	// B
 	public static final Item bad_touch = createItem("bad_touch",
 			new Item(new FabricItemSettings()
 					.group(VallHalla.VALLHALLA_TAB)
 					.maxCount(1)));
+	public static final Item beer = createItem("beer",
+			new Item(new FabricItemSettings()
+					.group(VallHalla.VALLHALLA_TAB)
+					.maxCount(1)));
+	public static final Item bleeding_jane = createItem("bleeding_jane",
+			new Item(new FabricItemSettings()
+					.group(VallHalla.VALLHALLA_TAB)
+					.maxCount(1)));
+	public static final Item bloom_light = createItem("bloom_light",
+			new Item(new FabricItemSettings()
+					.group(VallHalla.VALLHALLA_TAB)
+					.maxCount(1)));
+	public static final Item blue_fairy = createItem("blue_fairy",
+			new Item(new FabricItemSettings()
+					.group(VallHalla.VALLHALLA_TAB)
+					.maxCount(1)));
+	public static final Item brandtini = createItem("brandtini",
+			new Item(new FabricItemSettings()
+					.group(VallHalla.VALLHALLA_TAB)
+					.maxCount(1)));
+	// C
 	public static final Item cobalt_velvet = createItem("cobalt_velvet",
+			new Item(new FabricItemSettings()
+					.group(VallHalla.VALLHALLA_TAB)
+					.maxCount(1)));
+	public static final Item crevice_spike = createItem("crevice_spike",
+			new Item(new FabricItemSettings()
+					.group(VallHalla.VALLHALLA_TAB)
+					.maxCount(1)));
+	// F
+	public static final Item flaming_moai = createItem("flaming_moai",
 			new Item(new FabricItemSettings()
 					.group(VallHalla.VALLHALLA_TAB)
 					.maxCount(1)));
@@ -60,6 +91,33 @@ public class Items
 			new Item(new FabricItemSettings()
 					.group(VallHalla.VALLHALLA_TAB)
 					.maxCount(1)));
+	public static final Item frothy_water = createItem("frothy_water",
+			new Item(new FabricItemSettings()
+					.group(VallHalla.VALLHALLA_TAB)
+					.maxCount(1)));
+	// G
+	public static final Item grizzly_temple = createItem("grizzly_temple",
+			new Item(new FabricItemSettings()
+					.group(VallHalla.VALLHALLA_TAB)
+					.maxCount(1)));
+	public static final Item gut_punch = createItem("gut_punch",
+			new Item(new FabricItemSettings()
+					.group(VallHalla.VALLHALLA_TAB)
+					.maxCount(1)));
+	// M
+	public static final Item marsblast = createItem("marsblast",
+			new Item(new FabricItemSettings()
+					.group(VallHalla.VALLHALLA_TAB)
+					.maxCount(1)));
+	public static final Item murcuryblast = createItem("murcuryblast",
+			new Item(new FabricItemSettings()
+					.group(VallHalla.VALLHALLA_TAB)
+					.maxCount(1)));
+	public static final Item moonblast = createItem("moonblast",
+			new Item(new FabricItemSettings()
+					.group(VallHalla.VALLHALLA_TAB)
+					.maxCount(1)));
+	// P
 	public static final Item piano_man = createItem("piano_man",
 			new Item(new FabricItemSettings()
 					.group(VallHalla.VALLHALLA_TAB)
@@ -68,7 +126,12 @@ public class Items
 			new Item(new FabricItemSettings()
 					.group(VallHalla.VALLHALLA_TAB)
 					.maxCount(1)));
-	public static final Item piledriver = createItem("piledriver",
+	public static final Item pile_driver = createItem("pile_driver",
+			new Item(new FabricItemSettings()
+					.group(VallHalla.VALLHALLA_TAB)
+					.maxCount(1)));
+	// S
+	public static final Item sparkle_star = createItem("sparkle_star",
 			new Item(new FabricItemSettings()
 					.group(VallHalla.VALLHALLA_TAB)
 					.maxCount(1)));
@@ -79,7 +142,12 @@ public class Items
 	public static final Item sun_cloud = createItem("sun_cloud",
 			new Item(new FabricItemSettings()
 					.group(VallHalla.VALLHALLA_TAB)
-					.maxCount(20)));
+					.maxCount(1)));
+	public static final Item suplex = createItem("suplex",
+			new Item(new FabricItemSettings()
+					.group(VallHalla.VALLHALLA_TAB)
+					.maxCount(1)));
+	// Z
 	public static final Item zen_star = createItem("zen_star",
 			new Item(new FabricItemSettings()
 					.group(VallHalla.VALLHALLA_TAB)
